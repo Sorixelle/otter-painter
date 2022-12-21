7f8a3970cb086cda933f24c92991f512356548af
Ruby Iris Juric <ruby@srxl.me>
2022-12-21 21:21:01 +1100

    Create Otter component

---
+++ src/components/Otter.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import otterImages from './otterImages';
+
+  const src = otterImages[Math.floor(Math.random() * otterImages.length)]
+</script>
+
+<img {src} alt="Otter :)" />
---
+++ src/components/otterImages.ts
@@ -0,0 +1,5 @@
+export default [
+  "https://media.discordapp.net/attachments/1039260182271762464/1039280689914322954/16179581795_c0bce79876_z.jpg",
+  "https://media.discordapp.net/attachments/1039260182271762464/1039616910108663808/Screenshot_20221108-200445_Chrome.jpg",
+  "https://media.discordapp.net/attachments/1039260182271762464/1039880683126009916/f8b0d462-3e36-4ce9-b7ca-551ce6385bd0.jpg",
+]
