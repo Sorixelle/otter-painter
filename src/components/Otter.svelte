84ed3b8e64d6257e9d6cb612dee98614ddd2263b
Ruby Iris Juric <ruby@srxl.me>
2022-12-21 21:27:19 +1100

    Constrain otter image size

--- src/components/Otter.svelte
+++ src/components/Otter.svelte
@@ -4,4 +4,11 @@
   const src = otterImages[Math.floor(Math.random() * otterImages.length)]
 </script>
 
+<style>
+  img {
+    max-height: 450px;
+    max-width: 450px;
+  }
+</style>
+
 <img {src} alt="Otter :)" />
