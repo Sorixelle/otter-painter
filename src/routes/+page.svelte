87e6cc44db7b0ef72ef0614209facb126f8c6a68
Ruby Iris Juric <ruby@srxl.me>
2022-12-21 21:26:17 +1100

    Basic main page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,24 @@
   import Otter from "../components/Otter.svelte";
 </script>
 
-<Otter />
+<style>
+  .backdrop {
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .backdrop h1 {
+    font-family: sans-serif;
+    color: #bbb;
+  }
+</style>
+
+<div class="backdrop">
+  <h1>click anywhere for otter :3</h1>
+
+  <Otter />
+</div>
